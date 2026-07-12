05a564c1fd5305620db426cb281e7a69a0cf8dde
rkdehdrbs7885-oss <rkdehdrbs7885@gmail.com>
2026-07-12 16:11:27 +0900

    feat: 구독(Subscription) 엔티티 및 Enum 추가 (#16)

---
+++ src/main/java/com/mr/domain/subscriptions/enitity/Subscription.java
@@ -0,0 +1,34 @@
+package com.mr.domain.subscriptions.enitity;
+
+import com.mr.domain.subscriptions.enitity.enums.SubscriptionTier;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name="subscriptions")
+public class Subscription {
+
+    @Id
+    @Column(name = "subscription_id")
+    private Long id;
+
+    // 유저 아이디
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    // 구독 등급
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tier", nullable = false, length = 20)
+    private SubscriptionTier tier;
+
+    // 구독 시작일
+    @Column(name = "start_date", nullable = false)
+    private LocalDateTime startDate;
+
+    // 구독 종료일
+    @Column(name = "end_date", nullable = false)
+    private LocalDateTime endDate;
+}
---
+++ src/main/java/com/mr/domain/subscriptions/enitity/enums/SubscriptionTier.java
@@ -0,0 +1,7 @@
+package com.mr.domain.subscriptions.enitity.enums;
+
+public enum SubscriptionTier {
+    FREE,
+    STANDARD,
+    PRO
+}
