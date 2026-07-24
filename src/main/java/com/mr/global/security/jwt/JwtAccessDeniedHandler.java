65967dce7ff6bcbe7a066d54f108b725d22550ca
kimyw1018 <poppppp00@gmail.com>
2026-07-24 14:06:16 +0900

    feat: ObjectMapper주입 (#25)

--- src/main/java/com/mr/global/security/jwt/JwtAccessDeniedHandler.java
+++ src/main/java/com/mr/global/security/jwt/JwtAccessDeniedHandler.java
@@ -5,6 +5,7 @@ import com.mr.global.apipayload.ApiResponse;
 import com.mr.global.apipayload.code.CommonStatus;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
@@ -13,15 +14,16 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final ObjectMapper objectMapper;
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response,
                        AccessDeniedException accessDeniedException) throws IOException {
 
-        // 403 Forbidden 팀 공통 JSON 응답 반환
+        // 403 Forbidden 공통 JSON 응답 반환
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setCharacterEncoding("UTF-8");
         response.setStatus(HttpServletResponse.SC_FORBIDDEN);
--- src/main/java/com/mr/global/security/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/mr/global/security/jwt/JwtAuthenticationEntryPoint.java
@@ -5,6 +5,7 @@ import com.mr.global.apipayload.ApiResponse;
 import com.mr.global.apipayload.code.CommonStatus;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -13,15 +14,16 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final ObjectMapper objectMapper;
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
                          AuthenticationException authException) throws IOException {
 
-        // 401 Unauthorized 팀 공통 JSON 응답 반환
+        // 401 Unauthorized 공통 JSON 응답 반환
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setCharacterEncoding("UTF-8");
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
