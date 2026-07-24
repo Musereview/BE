f19f57cb820d81e6e5637d825221972b30e9c1bd
kimyw1018 <poppppp00@gmail.com>
2026-07-24 13:52:16 +0900

    feat: 앤드포인트 컨밴션 (#25)

--- src/main/java/com/mr/domain/auth/controller/AuthController.java
+++ src/main/java/com/mr/domain/auth/controller/AuthController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/api/auth")
 @Profile({"local", "dev"})
 public class AuthController {
 
