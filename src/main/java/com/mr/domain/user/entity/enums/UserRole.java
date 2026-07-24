c0e7b86e916987d03436178f4ddb3f44d316e3a5
kimyw1018 <poppppp00@gmail.com>
2026-07-24 14:15:41 +0900

    feat: 유저 role 이넘 생성 (#25)

---
+++ src/main/java/com/mr/domain/user/entity/enums/UserRole.java
@@ -0,0 +1,15 @@
+package com.mr.domain.user.entity.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserRole {
+    ROLE_STUDENT("ROLE_STUDENT", "학생"),
+    ROLE_TEACHER("ROLE_TEACHER", "강사"),
+    ROLE_ADMIN("ROLE_ADMIN", "관리자");
+
+    private final String key;
+    private final String title;
+}
