0ed59e6c1d98cf85de1baaf08cf61df7c80d90d1
kimyw1018 <poppppp00@gmail.com>
2026-07-24 13:34:30 +0900

    feat: 타입 캐스팅 로직 수정 (#25)

--- src/main/java/com/mr/global/security/SecurityUtil.java
+++ src/main/java/com/mr/global/security/SecurityUtil.java
@@ -2,6 +2,8 @@ package com.mr.global.security;
 
 import com.mr.global.apipayload.code.CommonStatus;
 import com.mr.global.apipayload.exception.GeneralException;
+import com.mr.global.security.principal.CustomUserDetails;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
@@ -13,14 +15,16 @@ public class SecurityUtil {
     public static Long getCurrentUserId() {
         final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        if (authentication == null || authentication.getName() == null || authentication.getName().equals("anonymousUser")) {
+        if (authentication == null
+                || !authentication.isAuthenticated()
+                || authentication instanceof AnonymousAuthenticationToken) {
             throw new GeneralException(CommonStatus.UNAUTHORIZED);
         }
 
-        try {
-            return Long.parseLong(authentication.getName());
-        } catch (NumberFormatException e) {
-            throw new GeneralException(CommonStatus.UNAUTHORIZED);
+        if (authentication.getPrincipal() instanceof CustomUserDetails userDetails) {
+            return userDetails.getUserId();
         }
+
+        throw new GeneralException(CommonStatus.UNAUTHORIZED);
     }
 }
