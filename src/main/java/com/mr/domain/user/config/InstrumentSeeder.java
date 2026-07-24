73b868449970ba10cd154ca6603d290dbdb3d77a
p1001q <qrwrer123@gmail.com>
2026-07-24 23:09:16 +0900

    fix: 시드 동시 실행 시 중복 저장 안전 처리 (#39)

--- src/main/java/com/mr/domain/user/config/InstrumentSeeder.java
+++ src/main/java/com/mr/domain/user/config/InstrumentSeeder.java
@@ -3,11 +3,14 @@ package com.mr.domain.user.config;
 import com.mr.domain.user.entity.Instrument;
 import com.mr.domain.user.repository.InstrumentRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class InstrumentSeeder implements ApplicationRunner {
@@ -20,7 +23,16 @@ public class InstrumentSeeder implements ApplicationRunner {
     @Override
     @Transactional
     public void run(ApplicationArguments args) {
-        instrumentRepository.findByCode(PIANO_CODE)
-                .orElseGet(() -> instrumentRepository.save(Instrument.create(PIANO_CODE, PIANO_NAME)));
+        if (instrumentRepository.findByCode(PIANO_CODE).isPresent()) {
+            return;
+        }
+
+        try {
+            instrumentRepository.save(Instrument.create(PIANO_CODE, PIANO_NAME));
+        } catch (DataIntegrityViolationException e) {
+            // 여러 인스턴스가 동시에 기동해 경쟁 상태로 중복 저장을 시도한 경우
+            // code unique 제약으로 한쪽만 성공하므로, 나머지는 무시하고 넘어감
+            log.info("PIANO 악기 시드 데이터가 다른 인스턴스에 의해 이미 생성됨");
+        }
     }
 }
