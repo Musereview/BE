e73b6cc25325cc8e77e795bb0de4954a59c6295e
ownue <enu_i@naver.com>
2026-07-25 17:03:35 +0900

    chore: 데드 코드 삭제

--- src/main/java/com/mr/domain/analysis/repository/AnalysisRepository.java
+++ src/main/java/com/mr/domain/analysis/repository/AnalysisRepository.java
@@ -1,10 +1,7 @@
 package com.mr.domain.analysis.repository;
 
 import com.mr.domain.analysis.entity.Analysis;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AnalysisRepository extends JpaRepository<Analysis, Long> {
-
-    Optional<Analysis> findByIdAndUserId(Long analysisId, Long userId);
 }
