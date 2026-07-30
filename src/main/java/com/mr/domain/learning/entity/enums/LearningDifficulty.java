04904fc3de3fc47ec5f34e213e915ea520e34904
p1001q <qrwrer123@gmail.com>
2026-07-30 20:14:48 +0900

    refactor: LearningDifficulty 생성자를 @AllArgsConstructor로 통일 (#79)

--- src/main/java/com/mr/domain/learning/entity/enums/LearningDifficulty.java
+++ src/main/java/com/mr/domain/learning/entity/enums/LearningDifficulty.java
@@ -1,16 +1,14 @@
 package com.mr.domain.learning.entity.enums;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public enum LearningDifficulty {
     BEGINNER("입문"),
     INTERMEDIATE("중급"),
     ADVANCED("고급");
 
     private final String label;
-
-    LearningDifficulty(String label) {
-        this.label = label;
-    }
 }
