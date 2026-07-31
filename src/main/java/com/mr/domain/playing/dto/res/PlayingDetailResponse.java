813604010c83b8394d87b6ab1831befb4903ccb4
onlyoneprivate <pllm098@naver.com>
2026-08-01 03:14:53 +0900

    fix: backingtrack 패키지 import 경로 수정 (#86)

--- src/main/java/com/mr/domain/playing/dto/res/PlayingDetailResponse.java
+++ src/main/java/com/mr/domain/playing/dto/res/PlayingDetailResponse.java
@@ -1,7 +1,7 @@
 package com.mr.domain.playing.dto.res;
 
-import com.mr.domain.backingTrack.entity.BackingTrack;
-import com.mr.domain.backingTrack.entity.enums.ScaleType;
+import com.mr.domain.backingtrack.entity.BackingTrack;
+import com.mr.domain.backingtrack.entity.enums.ScaleType;
 import com.mr.domain.playing.entity.Playing;
 import com.mr.domain.playing.entity.enums.PlayingMode;
 import com.mr.domain.playing.entity.enums.PlayingStatus;
