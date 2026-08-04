06a36f56fa9cea0359706e79cafc458a31b27a5b
onlyoneprivate <pllm098@naver.com>
2026-08-04 21:17:24 +0900

    style: 와일드카드 import 제거 (#132)

--- src/test/java/com/mr/domain/playing/service/PlayingServiceTest.java
+++ src/test/java/com/mr/domain/playing/service/PlayingServiceTest.java
@@ -52,9 +52,18 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class PlayingServiceTest {
